ecf0f5e75da3df026ef36c0b8cf949f4b0261f6b
Minh Nguyen <nhtminh2999@gmail.com>
2023-04-08 08:31:42 +0700

    add chat plugin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import 'antd/dist/antd.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './App.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/header/Header';
-import HeroSection from './components/hero_section/HeroSection';
-import IntroSection from './components/intro_section/IntroSection';
-import ServiceSection from './components/service_section/ServiceSection';
-import ReasonSection from './components/reason_section/ReasonSection';
-import ThingSection from './components/thing_section/ThingSection';
-import ContactSection from './components/contact_section/ContactSection';
-import LeaderSection from './components/leader_section/LeaderSection';
-import ResultSection from './components/result_section/ResultSection';
-import FeedbackSection from './components/feedback_section/FeedbackSection';
-import PartnerSection from './components/partner_section/PartnerSection';
-import NewsSection from './components/news_section/NewsSection';
-import Footer from './components/footer/Footer';
+import "antd/dist/antd.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./App.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/header/Header";
+import HeroSection from "./components/hero_section/HeroSection";
+import IntroSection from "./components/intro_section/IntroSection";
+import ServiceSection from "./components/service_section/ServiceSection";
+import ReasonSection from "./components/reason_section/ReasonSection";
+import ThingSection from "./components/thing_section/ThingSection";
+import ContactSection from "./components/contact_section/ContactSection";
+import LeaderSection from "./components/leader_section/LeaderSection";
+import ResultSection from "./components/result_section/ResultSection";
+import FeedbackSection from "./components/feedback_section/FeedbackSection";
+import PartnerSection from "./components/partner_section/PartnerSection";
+import NewsSection from "./components/news_section/NewsSection";
+import Footer from "./components/footer/Footer";
+import MessengerPlugin from "./components/messenger_plugin";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <PartnerSection />
       <NewsSection />
       <Footer />
+      <MessengerPlugin />
     </Router>
   );
 }
---
+++ src/components/messenger_plugin/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+
+const MessengerPlugin = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    ref.current.setAttribute("page_id", "your_page_id");
+    ref.current.setAttribute("attribution", "biz_inbox");
+
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v16.0",
+      });
+    };
+
+    (function (d, s, id) {
+      let js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
+
+  return (
+    <div>
+      <div id="fb-root" />
+      <div ref={ref} id="fb-customer-chat" className="fb-customerchat" />
+    </div>
+  );
+};
+
+export default MessengerPlugin;
